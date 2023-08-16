13d28fd9eb2141d6a6e14f232236b71edd567341
Jett Chen <jettchen12345@gmail.com>
2023-08-16 16:12:03 +0800

    Add AllContacts Endpoint

--- WeChatTweak/Alfred.m
+++ WeChatTweak/Alfred.m
@@ -95,6 +95,61 @@
         });
         return [GCDWebServerDataResponse responseWithJSONObject:@{@"items": items}];
     }];
+    [self.server addHandlerForMethod:@"GET" path:@"/wechat/allcontacts" requestClass:[GCDWebServerRequest class] processBlock:^GCDWebServerResponse * _Nullable(__kindof GCDWebServerRequest * _Nonnull request) {
+            
+        NSString *path = ({
+            NSString *path = nil;
+            if ([objc_getClass("PathUtility") respondsToSelector:@selector(GetCurUserDocumentPath)]) {
+                path = [objc_getClass("PathUtility") GetCurUserDocumentPath];
+            } else {
+                path = nil;
+            }
+            path;
+        });
+        
+        NSArray<WCContactData *> *contacts = ({
+            MMServiceCenter *serviceCenter = [objc_getClass("MMServiceCenter") defaultCenter];
+            ContactStorage *contactStorage = [serviceCenter getService:objc_getClass("ContactStorage")];
+            GroupStorage *groupStorage = [serviceCenter getService:objc_getClass("GroupStorage")];
+            NSMutableArray<WCContactData *> *array = [NSMutableArray array];
+            [array addObjectsFromArray:[contactStorage GetAllFriendContacts]];
+            [array addObjectsFromArray:[groupStorage GetGroupContactList:2 ContactType:0]];
+            array;
+        });
+
+        NSArray<NSDictionary<NSString *, id> *> *items = ({
+            NSMutableArray<NSDictionary<NSString *, id> *> *items = NSMutableArray.array;
+            for (WCContactData *contact in contacts) {
+                NSString *avatar = [NSString stringWithFormat:@"%@/Avatar/%@.jpg", path, [contact.m_nsUsrName md5String]];
+                BOOL isOfficialAccount = (contact.m_uiCertificationFlag >> 0x3 & 0x1) == 1;
+                if (!isOfficialAccount) {
+                    [items addObject:@{
+                        @"icon": @{
+                            @"path": [NSFileManager.defaultManager fileExistsAtPath:avatar] ? avatar : NSNull.null
+                        },
+                        @"title": ({
+                            id value = nil;
+                            if (contact.m_nsRemark.length) {
+                                value = contact.m_nsRemark;
+                            } else if (contact.m_nsNickName.length) {
+                                value = contact.m_nsNickName;
+                            } else {
+                                value = NSNull.null;
+                            }
+                            value;
+                        }),
+                        @"subtitle": contact.m_nsNickName.length ? contact.m_nsNickName : NSNull.null,
+                        @"arg": contact.m_nsUsrName.length ? contact.m_nsUsrName : NSNull.null,
+                        @"valid": @(contact.m_nsUsrName.length > 0)
+                    }];
+                }
+            }
+            items;
+        });
+
+        return [GCDWebServerDataResponse responseWithJSONObject:items];
+    }];
+
     // Start session
     [self.server addHandlerForMethod:@"GET" path:@"/wechat/start" requestClass:[GCDWebServerRequest class] processBlock:^GCDWebServerResponse * _Nullable(__kindof GCDWebServerRequest * _Nonnull request) {
         WCContactData *contact = ({
